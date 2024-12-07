efcdcac9ef5fac73558fe0bcd65ce5ac663adbd1
Qui Nguyen <qui@gmail.com>
2024-12-07 12:31:41 -0500

    first commit

--- script.js
+++ script.js
@@ -8,10 +8,22 @@ let ticketPrice = +movieSelect.value;
 
 // console.log(typeof ticketPrice);
 
+// Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrice", moviePrice);
+}
+
 // Update total and count
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
   //   console.log(selectedSeats);
+  // Copy selected seats into arr
+  // Map thru Array
+  // return a new array indexes
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+
   const selectedSeatsCount = selectedSeats.length;
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
@@ -20,6 +32,7 @@ function updateSelectedCount() {
 // Movie select event
 movieSelect.addEventListener("change", (e) => {
   ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
   //   console.log(ticketPrice);
   updateSelectedCount();
 });
